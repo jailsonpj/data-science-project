725aea1f668210b1943c4ded63a7ef7bc00ac070
jailsonpj <jailsoncolares@gmail.com>
2018-02-07 21:08:31 -0400

    coletando tweets com a palavra python no twitter

---
+++ pesquisatwitter/dadosTwitter.py
@@ -0,0 +1,13 @@
+from twython import Twython
+
+CONSUMER_KEY="QR5fsBnBCqD3Rda6szBchJCdW"
+CONSUMER_SECRET="13FYl5BN0QGd43X1qNhMPSZrjH2Virgo7uHKREv3aKPO3tmBKL"
+
+twitter = Twython(CONSUMER_KEY,CONSUMER_SECRET)
+
+#procura por tweets contendo a palavra python
+for status in twitter.search(q='"python"')["statuses"]:
+    user = status["user"]["screen_name"]
+    text = status["text"]
+    print(user,":",text)
+    print()
