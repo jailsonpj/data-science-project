7e743862e00dedd9033c578f3f66ebf0d0fe937e
jailsonpj <jailsoncolares@gmail.com>
2018-02-09 18:12:18 -0400

    atualizando

--- pesquisatwitter/dadosTwitter.py
+++ pesquisatwitter/dadosTwitter.py
@@ -1,9 +1,11 @@
 from twython import Twython
+import pandas as pd
 
 CONSUMER_KEY="QR5fsBnBCqD3Rda6szBchJCdW"
 CONSUMER_SECRET="13FYl5BN0QGd43X1qNhMPSZrjH2Virgo7uHKREv3aKPO3tmBKL"
 
 twitter = Twython(CONSUMER_KEY,CONSUMER_SECRET)
+search = {}
 
 #procura por tweets contendo a palavra python
 for status in twitter.search(q='"python"')["statuses"]:
@@ -11,10 +13,10 @@ for status in twitter.search(q='"python"')["statuses"]:
     text = status["text"]
     print(user,":",text)
     print()
-'''results = twitter.cursor(twitter.search,q='python')
 
-for result in results:
-    user = result["user"]["screen_name"]
-    text = result["text"]
-    print(user,":",text)
-    print()'''
+
+'''for status in twitter.search(q='"presidio manaus"')["statuses"]:
+    search = status
+
+df = pd.DataFrame(search)
+print(search)'''
