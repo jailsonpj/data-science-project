943ac545c6bce894d1691fdfc17d95028d3469b5
jailsonpj <jailsoncolares@gmail.com>
2018-02-10 13:47:27 -0400

    foi

--- pesquisatwitter/dadosTwitter.py
+++ pesquisatwitter/dadosTwitter.py
@@ -5,7 +5,7 @@ CONSUMER_KEY="QR5fsBnBCqD3Rda6szBchJCdW"
 CONSUMER_SECRET="13FYl5BN0QGd43X1qNhMPSZrjH2Virgo7uHKREv3aKPO3tmBKL"
 
 twitter = Twython(CONSUMER_KEY,CONSUMER_SECRET)
-search = {}
+
 
 #procura por tweets contendo a palavra python
 for status in twitter.search(q='"python"')["statuses"]:
